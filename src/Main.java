a04aafc9bd02eaaf44b71c0d949916c0dcd82b11
aLexa163-JV <alekcei-y@ya.ru>
2024-08-04 21:52:45 +0400

    Дополнительные задачи-Переменные 2

--- src/Main.java
+++ src/Main.java
@@ -1,126 +1,124 @@
 public class Main {
     public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+    }
 
-        System.out.println("Задача №1");
-        int num1 = 1147463647;
-        byte num2 = 125;
-        short num3 = 32666;
-        long num4 = 720368547758L;
-        float num5 = 65.36345233f;
-        double num6 = 945.52130;
-        System.out.println("Значение переменной num1 c типом int равно " + num1);
-        System.out.println("Значение переменной num2 c типом byte равно " + num2);
-        System.out.println("Значение переменной num3 c типом short равно " + num3);
-        System.out.println("Значение переменной num4 c типом long равно " + num4);
-        System.out.println("Значение переменной num5 c типом float равно " + num5);
-        System.out.println("Значение переменной num6 c типом double равно " + num6);
+    public static void task1() {
+/*
+    У вас есть две переменных, в которых хранятся два целочисленных числа.
+    Напишите программу, которая выводит в консоль результат деления первого числа на второе с остатком.
+    В качестве исходных данных используйте значения для первого числа – 47, для второго – 13)
+    В результате деления 47 /13
+    Выходит 3 и 8 в остатке
+*/
+        float num1 = 47;
+        float num2 = 13;
+        float num3 = num1 / num2;
+        System.out.println(num3);
+    }
 
-        //Задача №2
-        float number1 = 27.12f;
-        long number2 = 987678965549L;
-        double number3 = 2.786;
-        short number4 = 569;
-        short number5 = -159;
-        short number6 = 27897;
-        byte number7 = 67;
+    public static void task2() {
+/*
+    Напишите программу, которая вычисляет сумму цифр двухзначного числа.
+    Программа должна работать с любым значением числа.
+    В качестве исходных данных используйте число 44
+*/
+        int a = 44;
+        int b = a % 10;
+        int c = (a / 10) % 10;
+        System.out.println("Сумма цифр числа " + a + " равна " + (b + c));
+    }
 
-        System.out.println("Задача №3");
-        int totalStudentsLyudmila = 23;
-        int totalStudentsAnna = 27;
-        int totalStudentsEkaterina = 30;
-        int totalPaper = 480;
-        int eachStudentSheets = totalPaper / (totalStudentsLyudmila + totalStudentsAnna + totalStudentsEkaterina);
-        System.out.println("На каждого ученика рассчитано " + eachStudentSheets + " листов бумаги");
+    public static void task3() {
+/*
+    Напишите программу, которая вычисляет сумму цифр трехзначного числа.
+    Программа должна работать с любым значением числа. В качестве исходных данных используйте число 345.
+*/
+        int a = 345;
+        System.out.println("Сумма цифр числа " + a + " равна " + ((a % 10) + ((a / 10) % 10) + ((a / 100) % 10)));
+    }
 
-        System.out.println("Задача №4");
-        int machinePerformanceTwoMinutes = 16;
-        int machinePerformanceOneMinute = machinePerformanceTwoMinutes / 2;
-        int hoursDay = 24;
-        int minutesHour = 60;
-
-        String min20 = "20 минут";
-        int minutes20 = 20;
-        int performance20Minutes = minutes20 * machinePerformanceOneMinute;
-        System.out.println("За " + min20 + " машина произвела " + performance20Minutes + " штук бутылок");
-
-        String day1 = " сутки ";
-        int performanceDay = hoursDay * minutesHour * machinePerformanceOneMinute;
-        System.out.println("За" + day1 + "машина произвела " + performanceDay + " штук бутылок");
-
-        String day3 = " 3 дня ";
-        int quantityDays = 3;
-        int performanceThreeDays = quantityDays * hoursDay * minutesHour * machinePerformanceOneMinute;
-        System.out.println("За" + day3 + "машина произвела " + performanceThreeDays + " штук бутылок");
-
-        String oneMonth = " один месяц ";
-        int quantityDaysOneMonth = 31;
-        int performanceOneMonth = quantityDaysOneMonth * hoursDay * minutesHour * machinePerformanceOneMinute;
-        System.out.println("За" + oneMonth + "машина произвела " + performanceOneMonth + " штук бутылок");
-
-        System.out.println("Задача №5");
-        int totalCansPaint = 120;
-        int oneClassWhitePaint = 2;
-        int oneClassBrownPaint = 4;
-        int oneClassCans = oneClassWhitePaint + oneClassBrownPaint;
-        int totalClass = totalCansPaint / oneClassCans;
-        int totalCansWhitePaint = oneClassWhitePaint * totalClass;
-        int totalCansBrownPaint = oneClassBrownPaint * totalClass;
-        System.out.println("В школе, где " + totalClass + " классов, нужно " + totalCansWhitePaint + " банок белой краски и " + totalCansBrownPaint + " банок коричневой краски");
-
-        System.out.println("Задача №6");
-        byte totalBananas = 5; //почему подчеркивается волнистой линией?
-        byte oneBananaGr = 80;
-        short totalMilkMg = 200;
-        byte oneMilkMg = 100;
-        byte milk100Gr = 105;
-        byte totalIceCream = 2;
-        byte iceCreamGr = 100;
-        byte totalRawEggs = 4;
-        byte oneEggGr = 70;
-        int totalBananasGr = totalBananas * oneBananaGr;
-        int totalMilkGr = totalMilkMg / oneMilkMg * milk100Gr;
-        int totalIceCreamGr = totalIceCream * iceCreamGr;
-        int totalRawEggsGr = totalRawEggs * oneEggGr;
-        int totalGrams = totalBananasGr + totalMilkGr + totalIceCreamGr + totalRawEggsGr;
-        System.out.println(totalGrams + " гр");
-        float oneKg = 1000;
-        float Kg = totalGrams / oneKg;
-        System.out.println(Kg + " кг");
-
-        System.out.println("Задача №7");
-        byte needLoseKg = 7;
-        short oneKgGr = 1000;
-        int needLoseGr = needLoseKg * oneKgGr;
-        short needLoseDay250Gr = 250;
-        short needLoseDay500Gr = 500;
-        int totalDays250Gr = needLoseGr / needLoseDay250Gr;
-        int totalDays500Gr = needLoseGr / needLoseDay500Gr;
-        int averageDays = (totalDays250Gr + totalDays500Gr) / 2;
-        System.out.println("Если спортсмен будет худеть каждый день по 250 грамм, то на похудение уйдет " + totalDays250Gr + " дней ");
-        System.out.println("Если спортсмен будет худеть каждый день по 500 грамм, то на похудение уйдет " + totalDays500Gr + " дней ");
-        System.out.println("Чтобы добиться результата похудения, в среднем потребуется " + averageDays + " день");
-
-        System.out.println("Задача №8");
-        int monthlySalaryMasha = 67760;
-        int monthlySalaryDenis = 83690;
-        int monthlySalaryKristina = 76230;
+    public static void task4() {
+/*
+    Коля пришел в цветочный магазин и решил купить букет цветов своей подруге. Букет стоит ромашек 4899 рублей.
+    К цветам он купил открытку, которая стоит 157 рублей.
+    При покупке Коле дали скидку в 10% на сумму двух покупок.
+    Вычислите и выведите в консоль сумму двух покупок с учетом скидки.
+    Отдельно выведите сумму скидки двух покупок.
+ */
+        int flowers = 4899;
+        int postcard = 157;
         float tax = 10;
-        int months = 12;
-        int yrSalaryMasha = monthlySalaryMasha * months;
-        int yrSalaryDenis = monthlySalaryDenis * months;
-        int yrSalaryKristina = monthlySalaryKristina * months;
-        float monthlySalaryMashaTax = (tax / 100) * monthlySalaryMasha + monthlySalaryMasha;
-        float monthlySalaryDenisTax = (tax / 100) * monthlySalaryDenis + monthlySalaryDenis;
-        float monthlySalaryKristinaTax = (tax / 100) * monthlySalaryKristina + monthlySalaryKristina;
-        float yrSalaryMashaTax = monthlySalaryMashaTax * months;
-        float yrSalaryDenisTax = monthlySalaryDenisTax * months;
-        float yrSalaryKristinaTax = monthlySalaryKristinaTax * months;
-        float differenceSalaryMasha = yrSalaryMashaTax - yrSalaryMasha;
-        float differenceSalaryDenis = yrSalaryDenisTax - yrSalaryDenis;
-        float differenceSalaryKristina = yrSalaryKristinaTax - yrSalaryKristina;
-        System.out.println("Маша теперь получает " + monthlySalaryMashaTax + " рублей. Годовой доход вырос на " + differenceSalaryMasha + " рублей");
-        System.out.println("Денис теперь получает " + monthlySalaryDenisTax + " рублей. Годовой доход вырос на " + differenceSalaryDenis + " рублей");
-        System.out.println("Кристина теперь получает " + monthlySalaryKristinaTax + " рублей. Годовой доход вырос на " + differenceSalaryKristina + " рублей");
+        float discount = (tax / 100);
+        float amountPurchasesTax = (flowers + postcard) - (flowers + postcard) * discount;
+        System.out.println("Сумма двух покупок с учетом скидки = " + amountPurchasesTax + " рублей");
+        System.out.println("Сумм скидки двух покупок = " + (flowers + postcard) * discount + " рублей");
+    }
 
+    public static void task5() {
+/*
+    Сделайте расчет товаров со скидками.
+    а) Апельсины стоят 73 руб/кг и на них действует скидка 15%, а товар бананы стоят 95 руб/кг.
+    Клиент взял 10 кг апельсинов и 2кг бананов.
+    Вычислите и выведите в консоль сумму  покупки с учетом скидки.
+ */
+        int oranges = 73;
+        int orangesKg = 10;
+        int bananas = 95;
+        int bananasKg = 2;
+        float tax = 15;
+        float discount = (tax / 100);
+        float totalOranCash = (oranges * orangesKg) - (oranges * orangesKg) * discount;
+        int totalBananaCash = bananas * bananasKg;
+        System.out.println("Сумму  покупки с учетом скидки = " + (totalBananaCash + totalOranCash) + " рублей");
+
+/*
+    б) Шампанское стоит 2870 руб и на него действует скидка 50%, а товар хлеб стоит 27 руб.
+    Клиент взял 7 бутылок шампанского и 1 батон хлеба.
+    Вычислите и выведите в консоль сумму покупки с учетом скидки.
+ */
+        int champagne = 2870;
+        int bread = 27;
+        int quantityChampagne = 7;
+        float percent = 50;
+        float discount1 = (percent / 100);
+        float totalCash = champagne * quantityChampagne - (champagne * quantityChampagne) * discount1 + bread;
+        System.out.println("Сумму  покупки с учетом скидки = " + totalCash + " рублей");
+
+/*
+    в) Шампиньоны стоят 436 руб/кг, а белые грибы стоят 578 руб/кг и на них действует скидка 5%.
+    Клиент взял 2,5 кг шампиньонов и 3,75 кг белых грибов.
+    Вычислите и выведите в консоль сумму покупки с учетом скидки.
+ */
+        int champignons = 436;
+        int whiteMushroom = 578;
+        float percent1 = 5;
+        float quantityChampignons = 2.5f;
+        float quantityWhiteMushroom = 3.75f;
+        float disc = (percent1 / 100);
+        float cashChampignons = champignons * quantityChampignons;
+        float cashWhiteMushroom = whiteMushroom * quantityWhiteMushroom - (whiteMushroom * quantityWhiteMushroom) * disc;
+        System.out.println("Сумму  покупки с учетом скидки = " + (cashChampignons + cashWhiteMushroom) + " рублей");
+    }
+
+    public static void task6() {
+/*
+    Объявите 2 переменных типа double ****со значениями
+    x = 0.40, y = 0.87;
+    Посчитайте следующие выражения и выведите результат программы в консоль после каждого подсчета:
+    11*x + y
+    (x+10*y)- 15*(x/y)
+ */
+        double x = 0.40;
+        double y = 0.87;
+        double z = 11 * x + y;
+        double w = (x + 10 * y) - 15 * (x / y);
+        System.out.println(z);
+        System.out.println(w);
     }
 }
